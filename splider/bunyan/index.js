b8330a5ecb7a82ea086918158146a9463ac69a98
LaiHuanMin <lai_huanmin@163.com>
2017-09-19 11:24:48 +0800

    添加了logger，以及做好了最大页面值

--- splider/bunyan/index.js
+++ splider/bunyan/index.js
@@ -1,38 +1,6 @@
-var bunyan = require("bunyan");
-var path = require("path");
-var fs = require("fs");
-var moment = require("moment");
+var path = require('path')
+var createLogger = require(path.normalize(path.join(process.cwd(),"utils","createLogger","index.js")))
 
-var loggerPath = path.join(process.cwd(), "logger", "splider", moment().format("YYYY-MM-DD"));
-
-var log = bunyan.createLogger({
-  name: "splider",
-  streams: [
-    {
-      level: "info",
-      stream: process.stdout
-    },
-    {
-      level: "info",
-      path: path.normalize(loggerPath+"-info.log")
-    },
-    {
-      level: "warn",
-      stream: process.stdout
-    },
-    {
-      level: "warn",
-      path: path.normalize(loggerPath+"-warn.log")
-    },
-    {
-      level: "error",
-      stream: process.stderr
-    },
-    {
-      level: "error",
-      path: path.normalize(loggerPath+"-error.log")
-    }
-  ]
-});
+var log = createLogger("splider")
 
 module.exports = log;
