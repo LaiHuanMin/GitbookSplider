5289ff3836d077d5f0f14182b6dc250ba1bbdc11
LaiHuanMin <lai_huanmin@163.com>
2017-09-19 11:02:15 +0800

    添加了bunyan，以及获取到了最大页码值

--- splider/index.js
+++ splider/index.js
@@ -3,22 +3,14 @@ var log = require("./bunyan/");
 var runTask = require("./runTask/");
 var initTask = require("./initTask/");
 
-var { baseUrl, lang, langName } = urlConfig;
 
 global.AppUtil = {
   urlConfig,
   log
 };
 
-initTask();
-
-// request
-//   .get(baseUrl)
-//   .query({ lang, page: pageIndex })
-//   .end((err, res) => {
-//     if (err || !res.ok) {
-//       console.log("it's bad request");
-//     } else {
-//         console.log(process.cwd());
-//     }
-//   });
+initTask().then(maxPage=>{
+    log.info("initTask初始化完毕，本次任务最终的页码为"+maxPage);
+}).catch(fail=>{
+    log.error("initTask处错误产生：",JSON.stringify(fail));
+});
