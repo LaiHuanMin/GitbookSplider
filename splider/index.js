b3c88873c3c67e296c729d4bd04aea716e646636
LaiHuanMin <lai_huanmin@163.com>
2017-09-19 14:14:41 +0800

    完成爬虫初始化initTask的流程

--- splider/index.js
+++ splider/index.js
@@ -1,17 +1,22 @@
-var urlConfig = require("../config/url.json");
-var log = require("./bunyan/");
-var runTask = require("./runTask/");
-var initTask = require("./initTask/");
-var db = require("../mongoose/index.js")
+var urlConfig = require('../config/url.json')
+var log = require('./bunyan/')
+var runSpliderTask = require('./runTask/')
+var initSpliderTask = require('./initTask/')
+var initDbTask = require('../mongoose/index.js')
+
 
 global.SpliderUtil = {
   urlConfig,
-  log,
-  db
-};
+  log
+}
 
-initTask().then(maxPage=>{
-    log.info("initTask初始化完毕，本次任务最终的页码为"+maxPage);
-}).catch(fail=>{
-    log.error("initTask处错误产生：",JSON.stringify(fail));
-});
+//先初始化数据库，确保成功才继续下去
+initDbTask().then(() => {
+  initSpliderTask().then(maxPage => {
+    log.info('initSpliderTask初始化完毕，本次任务最终的页码为' + maxPage)
+  }).catch(fail => {
+    log.error('initSpliderTask处错误产生：', JSON.stringify(fail))
+  })
+}).catch(() => {
+  log.error('因为数据库产生错误，所以爬虫任务不再进行')
+})
